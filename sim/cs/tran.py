395b47aca0137739b5f442213af66cdb25078b1b
Carsten Wulff <carsten@wulff.no>
2024-09-15 15:06:43 +0200

    Update status

--- sim/cs/tran.py
+++ sim/cs/tran.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import yaml
 import cicsim as cs
+import random
+import os
+import string
 
 def main(name):
   yamlfile = name + ".yaml"
@@ -17,7 +20,15 @@ def main(name):
   obj["a_gain"] = obj["vpp_vout"]/obj["vpp_vg"]
   obj["a_gain_dB"] = float(20*np.log10(obj["a_gain"]))
 
-  cs.rawplot(name + ".raw","time","v(vg),v(vout)",ptype="same",fname=name +"_vin_vout.png")
+  #- use a random string to fool browser caches
+  rss = ""
+  if(os.environ.get("CIC_RANDOM_PLOTNAME")):
+    rss = "_" + "".join(random.choice(string.ascii_uppercase+string.digits) for _ in range(5))
+
+
+  fname = name + rss + "_vin_vout.png"
+  print(f"Saving {fname}")
+  cs.rawplot(name + ".raw","time","v(vg),v(vout)",ptype="same",fname=fname)
 
   # Save new yaml file
   with open(yamlfile,"w") as fo:
